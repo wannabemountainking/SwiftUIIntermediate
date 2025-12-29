03b2df4d029b809d6b6f4b94e67cb63379df8b62
김도윤 <kdy0096@gmail.com>
2025-12-29 20:59:29 +0900

    비동기

---
+++ SwiftUIIntermediate/SwiftUIGrammar/WeakSelfInter/WeakSelfInter.swift
@@ -0,0 +1,18 @@
+//
+//  WeakSelfInter.swift
+//  SwiftUIIntermediate
+//
+//  Created by yoonie on 12/29/25.
+//
+
+import SwiftUI
+
+struct WeakSelfInter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeakSelfInter()
+}
